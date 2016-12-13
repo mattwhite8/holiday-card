0a46b44cad2ff56ee252d757f1c4c5665f548124
Matt White <mattwhite1337@gmail.com>
2016-12-13 12:08:06 -0500

    finally got picture to sit over div

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,8 @@ var showToFrom = false;
 
 document.getElementById("photo").addEventListener("click", function() {
 	document.getElementById("photo").style.display = "none";
-	
 });
+
+// document.getElementById("show-pic").addEventListener("click", function() {
+// 	document.getElementById("photo").style.display = "";
+// });
