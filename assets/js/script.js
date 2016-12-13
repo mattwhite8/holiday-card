cace4ee51ce6d59d7ffef0568dbb04907fa444e9
Matt White <mattwhite1337@gmail.com>
2016-12-13 12:50:57 -0500

    almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-var showToFrom = false;
+	var to = "";
+	var content = "";
+	var fromWho = "";
+	var toHTML = document.getElementById("to");
+	var contentHTML = document.getElementById("content");
+	var fromHTML = document.getElementById("from");
 
-document.getElementById("photo").addEventListener("click", function() {
-	document.getElementById("photo").style.display = "none";
+	to = prompt("Who shall I send the card to?");
+	content = prompt("What do you want to say?");
+	fromWho = prompt("Who is this card from?");
+
+	document.getElementById("photo").addEventListener("click", function() {
+		document.getElementById("photo").style.display = "none";
+		toHTML.innerHTML = "To: " + to;
+		contentHTML.innerHTML = content;
+		fromHTML.innerHTML = "From: " + fromWho;
+	});
+
 });
 
-// document.getElementById("show-pic").addEventListener("click", function() {
-// 	document.getElementById("photo").style.display = "";
-// });
+
