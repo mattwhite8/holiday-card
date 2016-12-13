d9bc7e151a40d9b9498bb4dee4be30919e2bb1c9
Matt White <mattwhite1337@gmail.com>
2016-12-13 17:38:42 -0500

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var contentHTML = document.getElementById("content");
 	var fromHTML = document.getElementById("from");
 
-	to = prompt("Who shall I send the card to?");
-	content = prompt("What do you want to say?");
-	fromWho = prompt("Who is this card from?");
+
 
 	document.getElementById("photo").addEventListener("click", function() {
+		to = prompt("Who shall I send the card to?");
+		content = prompt("What do you want to say?");
+		fromWho = prompt("Who is this card from?");
 		document.getElementById("photo").style.display = "none";
 		toHTML.innerHTML = "To: " + to;
 		contentHTML.innerHTML = content;
